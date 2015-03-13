2fa75dc8d33df6d2adc614674e42ac673aca8488
Pierre-Alexis de Solminihac <pa@quai13.com>
2015-03-13 20:54:33 +0100

    BOOTSTRAP3DATETIMEPICKER 1.0

---
+++ ctrl/bootstrap3datetimepickerCrudController.php
@@ -0,0 +1,46 @@
+<?php
+class bootstrap3datetimepickerCrudController extends bootstrap3datetimepickerCrudController_Parent
+{
+    public function override_fields_create_or_update($request, $params = null)
+    {
+        $ret = parent::override_fields_create_or_update($request, $params);
+        $this->data['params_datepicker_date'] = array(
+            'locale' => "'fr'",
+            'useCurrent' => 'false',
+            'format' => "'DD/MM/YYYY'",
+        );
+        $this->data['params_datepicker_time'] = array(
+            'locale' => "'fr'",
+            'useCurrent' => 'false',
+            'format' => "'HH:mm'",
+        );
+        $this->data['params_datepicker_datetime'] = array(
+            'locale' => "'fr'",
+            'useCurrent' => 'false',
+            'format' => "'DD/MM/YYYY HH:mm'",
+        );
+        return $ret;
+    }
+
+    public function register_ui_scripts($mode = 'index')
+    {
+        $ret = parent::register_ui_scripts($mode);
+        $cssjs = $this->getModel('cssjs');
+        if (in_array($mode, array('create', 'update'))) {
+            $cssjs->register_css('bootstrap3datetimepicker', array(
+                'src' => __WWW_ROOT_BOOTSTRAP3DATETIMEPICKER__ . '/skin/css/bootstrap-datetimepicker.min.css'
+            ));
+            $cssjs->register_css('clementine-bootstrap3datetimepicker', array(
+                'src' => __WWW_ROOT_BOOTSTRAP3DATETIMEPICKER__ . '/skin/css/clementine-bootstrap-datetimepicker.css'
+            ));
+            $cssjs->register_foot('momentjs', array(
+                'src' => __WWW_ROOT_MOMENTJS__ . '/skin/js/moment-with-locales.min.js'
+            ));
+            $cssjs->register_foot('bootstrap3datetimepicker', array(
+                'src' => __WWW_ROOT_BOOTSTRAP3DATETIMEPICKER__ . '/skin/js/bootstrap-datetimepicker.js'
+            ));
+        }
+        return $ret;
+    }
+
+}
---
+++ view/crud/js_datepicker.php
@@ -0,0 +1,61 @@
+<?php
+$config = Clementine::$config['module_bootstrap3datetimepicker'];
+$this->getParentBlock($data, $request);
+$input_types = array(
+    'date'      => 'YYYY-MM-DD',
+    'time'      => 'HH:mm',
+    'datetime'  => 'YYYY-MM-DD HH:mm',
+);
+?>
+<script>
+jQuery(document).ready(function() {
+<?php
+foreach ($input_types as $type => $shadow_format) {
+?>
+
+    // input date time fallback
+    jQuery('.clementine_crud-create_type-<?php echo $type; ?>, .clementine_crud-update_type-<?php echo $type; ?>').each(function() {
+        // si pas de support natif, on remplace
+        var shadow_el = jQuery('#' + jQuery(this).attr('id') + '-hidden');
+        var shadow_format = '<?php echo $shadow_format; ?>';
+        var datevalue = '';
+        var datevalue_shadow_formatted = '';
+        var shadow_el_val = shadow_el.val();
+        if (shadow_el_val != '' && shadow_el_val != '0000-00-00' && shadow_el_val != '0000-00-00 00:00' && shadow_el_val != '0000-00-00 00:00:00') {
+            datevalue = moment(shadow_el_val, shadow_format);
+            datevalue_shadow_formatted = moment(datevalue).format(shadow_format);
+        }
+        if (<?php echo $config['force_crud_datepicker'] . ' || '; ?>jQuery(this).prop('type') == 'text') {
+            jQuery(this).prop('type', 'text');
+            jQuery(this).val('');
+            jQuery(this).datetimepicker({
+<?php
+    foreach ($data['params_datepicker_' . $type] as $param_key => $param_val) {
+        echo $param_key . ': ' . $param_val . ',' . PHP_EOL;
+    }
+?>
+                defaultDate: datevalue
+            });
+            // desactive le handler actuel, on va le remplacer
+            jQuery(this).off('change.<?php echo $data['class']; ?>_datepicker, focus.<?php echo $data['class']; ?>_datepicker, blur.<?php echo $data['class']; ?>_datepicker');
+            jQuery(this).on('dp.change', function(e) {
+                var el_id = e.target.id;
+                var value = '';
+                var moment = e.date;
+                value = '';
+                if (moment) {
+                    value = moment.format(shadow_format);
+                }
+                //MAJ le champ shadow
+                crud_js_datepicker_update_shadow(el_id, value);
+            });
+        } else {
+            jQuery(this).val(datevalue_shadow_formatted);
+        }
+    });
+
+<?php
+}
+?>
+});
+</script>
